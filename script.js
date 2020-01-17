de642439373611579bf6f79ee656636bf7f4039f
Glitch (nebula-allspice) <none>
2020-01-17 22:13:30 +0000

    💎⛈ Checkpoint ./script.js:471185/1247

--- script.js
+++ script.js
@@ -4,10 +4,28 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 
-function setup() {
+var cells = [1,0,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0,0];
+let w = 10;
 
+function draw_row(row_num, cells) {
+    for (var i = 0; i < cells.length; i++) {
+        console.log(i);
+        if (cells[i] == 0) { fill(255); }
+        else { fill(0); }
+        stroke(0);
+        rect(i*w,row_num*w,w,w);
+        console.log(i*50);
+    }
+}
+
+function add_row() {
+    for 
+}
+
+function setup() {
+    createCanvas(640, 480);
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80);
+    draw_row(0, cells);
 }
