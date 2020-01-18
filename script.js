e6082db02d26f38052d8fb3128e688d7ecce6f62
Glitch (nebula-allspice) <none>
2020-01-18 03:28:33 +0000

    ⏲📻 Checkpoint ./script.js:471185/1465 ./index.html:471185/104 ./style.css:471185/1409 ./.glitch-assets:471185/335

--- script.js
+++ script.js
@@ -4,12 +4,25 @@
 // prints "hi" in the browser's dev tools console
 console.log("hi");
 
-var cells = [0,0,1,0,0,0,0,1,0,1,1,1,0,0,0,1,1,1,0,0];
-let w = 5;
-let ruleset = [0,1,0,1,0,0,1,0].reverse();
+let w = 10;
+
+function to_binary(n) {
+  var b = n.toString(2).split('');
+  for (var i=b.length; i<8;i++) {
+    b.unshift(0)
+  }
+  for (var i=0; i<b.length; i++) {
+    b[i] = parseInt(b[i]);
+  }
+  b.reverse();
+  return b;
+}
+
+var ruleset = to_binary(12);
+var cells;
 
 function draw_row(row_num, cells) {
-    for (var i = 1; i < cells.length - 1; i++) {
+    for (var i = 0; i < cells.length - 0; i++) { // change this back to 1 to remove first and last
         if (cells[i] == 0) { fill(255); }
         else { fill(0); }
         stroke(0);
@@ -24,7 +37,7 @@ function rules(left, middle, right) {
 }
 
 function add_row(cells) {
-    var newrow = [0]; // blank first cell
+    var newrow = Array(cells.length);
     for (var i=1; i < cells.length-1; i++) {
         var left = cells[i-1];
         var middle = cells[i];
@@ -32,19 +45,17 @@ function add_row(cells) {
         var newstate = rules(left, middle, right);
         newrow[i] = newstate;
     }
-    newrow[newrow.length] = 0; // blank last cell
     return newrow;
 }
 
 function setup() {
   createCanvas(windowWidth - 40, windowHeight);
-  var gap = Math.floor(((width - (cells.length * w)) / w)/2);
-  if (gap > 0) {
-    for (var i=0; i < gap; i++) {
-      cells.unshift(0);
-      cells.push(0);
-    }
+  var blockWidth = Math.floor(windowWidth/w);
+  cells = Array(blockWidth);
+  for (var i=0; i<cells.length; i++) {
+    cells[i] = 0;
   }
+  cells[Math.floor(blockWidth / 2)] = 1;
   noLoop();
 }
 
